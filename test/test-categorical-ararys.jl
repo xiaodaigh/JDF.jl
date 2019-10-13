cc0a615bad3d5a894ae3ac1566e1c78729e7594a
evalparse <zhuojia.dai@gmail.com>
2019-10-14 00:47:30 +1100

    ability to select columns

--- test/test-categorical-ararys.jl
+++ test/test-categorical-ararys.jl
@@ -6,13 +6,13 @@ using JDF
     df = DataFrame([collect(1:100) for i =1:3])
     df[!, :x1] = categorical(df[!, :x1])
     df[!, :x2] = categorical(string.(df[!, :x2]))
-    savejdf("a2cate.jdf", df)
+    savejdf("a3cate.jdf", df)
 
-    df2 = loadjdf("a2cate.jdf")
+    df2 = loadjdf("a2cate.jdf", cols=[:x2, :x3])
     @test size(df2, 2) == 3
     @test size(df2, 1) == 100
     @time df2[!, :x1] isa CategoricalVector{Int}
     @time df2[!, :x2] isa CategoricalVector{String}
 
-    rm("a2cate.jdf", force=true, recursive=true)
+    rm("a3cate.jdf", force=true, recursive=true)
 end
