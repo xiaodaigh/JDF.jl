30e9f5ded02318bf31c590f3c9c15fa6adc42408
evalparse <zhuojia.dai@gmail.com>
2019-10-17 22:30:38 +1100

    fixed bug where CSV.jl read files are not save-able

--- src/compress_then_write.jl
+++ src/compress_then_write.jl
@@ -1,5 +1,5 @@
 # the generic dispatch
-compress_then_write(b, io) = compress_then_write(eltype(b), b, io)
+compress_then_write(b::AbstractArray, io) = compress_then_write(eltype(b), Vector(b), io)
 
 # the generic dispatch for T where is isbits
 compress_then_write(T, b, io) = begin
