a3906d84e9fa91b96c19cc3be1e4215fd91fa7a0
evalparse <zhuojia.dai@gmail.com>
2019-10-19 23:05:32 +1100

    fixed final stringarry bug

--- src/type-writer-loader/StringArray.jl
+++ src/type-writer-loader/StringArray.jl
@@ -1,61 +1,21 @@
 compress_then_write(b::StringVector{T}, io) where {T} = begin
 
-    #println("ok")
-    # bufferc = Blosc.compress(b.buffer )
-    # buffer_meta  = (eltype(b.buffer) ,  write(io, bufferc))
-    #lengths_meta = (eltype(b.lengths),  write(io, Blosc.compress(b.lengths)))
-    #offsets_meta = (eltype(b.offsets),  write(io, Blosc.compress(b.offsets)))
-    bbc = Blosc.compress(b.buffer)
-    len1 = length(bbc)
-    len2 = write(io, bbc)
-    (b.buffer, bbc, len1, len2)
-    # (
-    #  #metadata = (buffer = buffer_meta, lengths = lengths_meta, offsets = offsets_meta),
-    #  metadata = (buffer = (type=UInt8, len = res)),
-    #  type = typeof(b),
-    #  bufferc = bbc
-    # )
+    buffer_meta  = (type=eltype(b.buffer) ,  len=write(io, Blosc.compress(b.buffer)))
+    offsets_meta = (type=eltype(b.offsets),  len=write(io, Blosc.compress(b.offsets)))
+    lengths_meta = (type=eltype(b.lengths),  len=write(io, Blosc.compress(b.lengths)))
+
+    (
+        metadata = (buffer = buffer_meta, offsets = offsets_meta, lengths = lengths_meta),
+        type = typeof(b),
+    )
 end
 
 column_loader(::Type{StringVector{T}}, io, metadata) where {T} = begin
-    args = Any[]
+    buffer  = column_loader(metadata.metadata.buffer.type , io, metadata.metadata.buffer)
+    offsets = column_loader(metadata.metadata.offsets.type, io, metadata.metadata.offsets)
+    lengths = column_loader(metadata.metadata.lengths.type, io, metadata.metadata.lengths)
 
-    # assign the buffer once
-    #buffer = Vector{UInt8}(undef, maximum(x -> x[2], metadata.metadata))
-
-    buffer = Vector{UInt8}(undef, metadata.metadata.buffer[2])
-    return metadata.metadata.buffer
-    readbytes!(io, buffer, metadata.metadata.buffer[2])
-    return Blosc.decompress(metadata.metadata.buffer[1], buffer)
-    # for (elm_type, compressed_bytes) in metadata.metadata
-    #     println(elm_type, compressed_bytes)
-    #     buffer = Vector{UInt8}(undef, compressed_bytes)
-    #     readbytes!(io, buffer, compressed_bytes)
-    #     println(length(buffer))
-    #     res = Blosc.decompress(elm_type, buffer)
-    #     return res
-    #     push!(args, res)
-    # end
-    return args
-    metadata.type(args...)
+    metadata.type(buffer, offsets, lengths)
 end
 
-if false
-using Revise
-using WeakRefStrings, Blosc, JDF, DataFrames
-
-a = StringVector(["a", "b", "a", missing, "c"])
-io = open("c:/data/test.io", "w")
-metadata = compress_then_write(a, io)
-close(io)
-
-io = open("c:/data/test.io", "r")
-aa = column_loader(StringVector{String}, io, metadata)
-close(io)
-
-df = DataFrame(a = a)
-
-savejdf("c:/data/pls_del.jdf", df)
-
-loadjdf("c:/data/pls_del.jdf")
-end
+# tests at test/test-stringarray.jl
