1540adb85a328f93b2ece7142eedc82dcefffc07
evalparse <zhuojia.dai@gmail.com>
2019-10-20 13:04:27 +1100

    minor update to test

--- test/test-stringarray.jl
+++ test/test-stringarray.jl
@@ -4,17 +4,20 @@ using JDF, WeakRefStrings, DataFrames
 
 @testset "JDF.jl WeakRefStrings.StringArrays" begin
     a = StringVector(["a", "b", "a", missing, "c"])
-    io = open("test.io", "w")
+    io = open("io.jdf", "w")
     metadata = compress_then_write(a, io)
     close(io)
 
-    io = open("test.io", "r")
+    io = open("io.jdf", "r")
     aa = column_loader(StringVector{String}, io, metadata)
     close(io)
 
     df = DataFrame(a = a)
     savejdf("pls_del.jdf", df)
-    loadjdf("pls_del.jdf")
+    df2 = loadjdf("pls_del.jdf")
 
+    
+
+    rm("io.jdf", force=true)
     rm("pls_del.jdf", force=true, recursive=true)
 end
