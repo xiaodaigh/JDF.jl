7acdf1febe1ad0fb048ff530c1c5f09413adca64
evalparse <zhuojia.dai@gmail.com>
2019-10-21 21:50:08 +1100

    more docstrings, and change string compression algorithm to exact

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,7 +8,12 @@ include("test-stringarray.jl")
 
 @testset "JDF.jl parallel" begin
     df = DataFrame([collect(1:100) for i =1:3000])
-    df[!, :int_missing] =  rand([rand(rand([UInt, Int, Float64, Float32, Bool])), missing], nrow(df))
+    df[!, :int_missing] =
+        rand(
+            [rand(rand([UInt, Int, Float64, Float32, Bool])), missing],
+            nrow(df)
+        )
+        
     df[!, :missing] .= missing
     df[!, :strs] = [randstring(8) for i in 1:nrow(df)]
     df[!, :stringarray] = StringVector([randstring(8) for i in 1:nrow(df)])
