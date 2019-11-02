06cefc4e0eee7bca9d7f95cc7ad24540ef6a188f
evalparse <zhuojia.dai@gmail.com>
2019-11-03 01:57:52 +1100

    ready to merge timezones

--- src/type-writer-loader/ZonedDateTime.jl
+++ src/type-writer-loader/ZonedDateTime.jl
@@ -1,13 +1,33 @@
-# save symbols
+# ##############################################################################
+# ZonedDateTime
+# ##############################################################################
+# the structure
+  # utc_datetime : isbits
+  # timezone : VariableTimeZone
+  #   name : string
+  #   transitions : Vector{TimeZones.Transitions}
+  #     utc_datetime : isbits
+  #     zone: FixedTimeZone
+  #       name : string
+  #       offset : isbits
+  #   cutoff : isbits (Union{Nothing, DateTime})
+  # zone
+  #   name : string
+  #   offset : isbits
 compress_then_write(b::Vector{ZonedDateTime}, io) = begin
-    string_meta = compress_then_write(String.(b), io)
-    (type = ZonedDateTime, len = string_meta.len, metadata = string_meta)
+  utc_datetime_meta = compress_then_write([b.utc_datetime for b in b], io)
+  timezone_meta = compress_then_write([b.timezone for b in b], io)
+  zone_meta = compress_then_write([b.zone for b in b], io)
+
+  (type = ZonedDateTime, len = utc_datetime_meta.len + timezone_meta.len + zone_meta.len, utc_datetime_meta = utc_datetime_meta, timezone_meta = timezone_meta, zone_meta = zone_meta)
 end
 
 # load a ZonedDateTime column
 column_loader(::Type{ZonedDateTime}, io, metadata) = begin
-    strs = column_loader(String, io, metadata.metadata)
-    ZonedDateTime.(strs)
+  v1 = column_loader(metadata.utc_datetime_meta.type, io, metadata.utc_datetime_meta)
+  v2 = column_loader(metadata.timezone_meta.type, io, metadata.timezone_meta)
+  v3 = column_loader(metadata.zone_meta.type, io, metadata.zone_meta)
+  [ZonedDateTime(v1, v2, v3) for (v1, v2, v3) in zip(v1, v2, v3)]
 end
 
 column_loader!(_, ::Type{ZonedDateTime}, io, metadata) = begin
@@ -16,56 +36,97 @@ end
 
 some_elm(::Type{ZonedDateTime}) = ZonedDateTime(1984, 1, 5, tz"Asia/Shanghai")
 
-if false
-a = some_elm(ZonedDateTime)
+# ##############################################################################
+# TimeZones.Transitions
+# ##############################################################################
+compress_then_write(b::Vector{TimeZones.Transition}, io) = begin
+  # the structure of Transition is like
+  # (utc_datetime::isbits, zone::string)
+  utc_datetime_meta = compress_then_write([b.utc_datetime for b in b], io)
+  zone_meta = compress_then_write([b.zone for b in b], io)
 
-ar = Vector{ZonedDateTime}(undef, 10)
-ar .= a
-
-fieldnames(typeof(a))
-
-typeof(a.utc_datetime)
-typeof(a.utc_datetime) |> isbits
-
-fieldnames(typeof(a.utc_datetime))
-
-a.utc_datetime.instant
-a.utc_datetime.instant |> isbits
-
-a1 = map(ar) do x
-    x.utc_datetime
+  (type = TimeZones.Transition, len = utc_datetime_meta.len + zone_meta.len, utc_datetime_meta = utc_datetime_meta, zone_meta = zone_meta)
 end
 
-a1[1] |> isbits
-
-open("c:/data/plsdel.io", "w") do io
-    compress_then_write(a1, io)
+column_loader(::Type{TimeZones.Transition}, io, metadata) = begin
+  v1 = column_loader(metadata.utc_datetime_meta.type, io, metadata.utc_datetime_meta)
+  v2 = column_loader(metadata.zone_meta.type, io, metadata.zone_meta)
+  [TimeZones.Transition(v1, v2) for (v1, v2) in zip(v1, v2)]
 end
 
-typeof(a.timezone)
-typeof(a.timezone) |> isbits
-fieldnames(a.timezone |> typeof)
+column_loader!(::Type{TimeZones.Transition}, io, metadata) = begin
+    column_loader(TimeZones.Transition, io, metadata)
+end
 
-a.timezone.name
+# ##############################################################################
+# VariableTimeZone
+# ##############################################################################
+compress_then_write(b::Vector{VariableTimeZone}, io) = begin
+  # the structure of Transition is like
+  # (name::string, transitions, cutoff::string)
+  name_meta = compress_then_write([b.name for b in b], io)
 
-typeof(a.timezone.name)
+  len_transitions = [length(b.transitions) for b in b]
+  len_transitions_meta = compress_then_write(len_transitions, io)
 
-typeof(a.timezone.transitions)
-typeof(a.timezone.transitions) |> isbits
-a.timezone.transitions[1]
-a.timezone.transitions[1] |> isbits
-fieldnames(typeof(a.timezone.transitions[1]))
+  transitions = Vector{TimeZones.Transition}(undef, sum(len_transitions))
+  i = 1
+  for b in b
+    for t in b.transitions
+      transitions[i] = t
+      i += 1
+    end
+  end
+  transitions_meta = compress_then_write(transitions, io)
+  cutoff_meta = compress_then_write([b.cutoff for b in b], io)
 
-a.timezone.transitions[1].utc_datetime
-a.timezone.transitions[1].utc_datetime |> typeof
-a.timezone.transitions[1].zone |> typeof
+  (
+    type = VariableTimeZone,
+    len = name_meta.len + len_transitions_meta.len + transitions_meta.len + cutoff_meta.len,
+    name_meta = name_meta, len_transitions_meta = len_transitions_meta,
+    transitions_meta = transitions_meta, cutoff_meta = cutoff_meta
+  )
+end
 
-typeof(a.timezone.cutoff)
-typeof(a.timezone.cutoff) |> isbits
+column_loader(::Type{VariableTimeZone}, io, metadata) = begin
+  name = column_loader(metadata.name_meta.type, io, metadata.name_meta)
+
+  len_transitions = column_loader(metadata.len_transitions_meta.type, io, metadata.len_transitions_meta)
+  hi = cumsum(len_transitions)
+  lo = vcat(0, hi[1:end-1]) .+ 1
 
 
-typeof(a.zone)
-typeof(a.zone) |> isbits
+  transitions = column_loader(metadata.transitions_meta.type, io, metadata.transitions_meta)
+
+  metadata.cutoff_meta.type
+  cutoff = column_loader(metadata.cutoff_meta.type, io, metadata.cutoff_meta)
 
 
+  [VariableTimeZone(name, transitions[lo:hi], cutoff) for (name, lo, hi, cutoff) in zip(name, lo, hi, cutoff)]
+end
+
+column_loader!(::Type{VariableTimeZone}, io, metadata) = begin
+    column_loader(VariableTimeZone, io, metadata)
+end
+
+# ##############################################################################
+# FixedTimeZone
+# ##############################################################################
+compress_then_write(b::Vector{FixedTimeZone}, io) = begin
+  # the structure of Transition is like
+  # (name, offset)
+  m1 = compress_then_write([b.name for b in b], io)
+  m2 = compress_then_write([b.offset for b in b], io)
+
+  (type = FixedTimeZone, len = m1.len + m2.len, name_meta = m1, offset_meta = m2)
+end
+
+column_loader(::Type{FixedTimeZone}, io, metadata) = begin
+  v1 = column_loader(metadata.name_meta.type, io, metadata.name_meta)
+  v2 = column_loader(metadata.offset_meta.type, io, metadata.offset_meta)
+  [FixedTimeZone(v1, v2) for (v1, v2) in zip(v1, v2)]
+end
+
+column_loader!(::Type{FixedTimeZone}, io, metadata) = begin
+    column_loader(FixedTimeZone, io, metadata)
 end
